028b2d15b602d27a5b968b036f3a5fd52739749e
Dmitrii Kharitonov <dimds@fastmail.com>
2025-07-31 22:20:57 +0200

    test must be reworked completely

--- backend/src/repositories/mock/index.ts
+++
@@ -1,6 +0,0 @@
-// Mock repository implementations for testing
-
-export { MockAnswerRepository } from "./mockAnswerRepository.js";
-export { MockExperienceRepository } from "./mockExperienceRepository.js";
-export { MockInterviewRepository } from "./mockInterviewRepository.js";
-export { MockTopicRepository } from "./mockTopicRepository.js";
