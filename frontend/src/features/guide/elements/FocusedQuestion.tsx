9ff32da418ef25eb4854eabe7925a81cf1cbc909
Dmitrii Kharitonov <dimds@fastmail.com>
2025-08-17 12:11:39 +0200

    fix: add width to question indicator for consistent layout

--- frontend/src/features/guide/elements/FocusedQuestion.tsx
+++ frontend/src/features/guide/elements/FocusedQuestion.tsx
@@ -7,7 +7,7 @@ interface FocusedQuestionProps {
 const FocusedQuestion = ({ question, number: _number, total: _total }: FocusedQuestionProps) => {
   return (
     <div className="mt-16 flex items-baseline space-x-6">
-      <div className="flex-shrink-0 flex justify-center text-headline font-serif font-medium text-secondary">Q</div>
+      <div className="flex-shrink-0 w-7 flex justify-center text-headline font-serif font-medium text-secondary">Q</div>
       <div className="flex-grow">
         <h2 className="text-headline font-medium text-primary leading-snug">
           {question}
